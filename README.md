0b9682819dd4bc23b8269fe8120ec6fb74e6aa0c
zero <156142060@qq.com>
2020-03-22 03:52:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+some vue balabala
---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p v-html="rawHtml"></p>
+    <div v-bind:class="color">test..d</div>
+    <p>{{number+1}}</p>
+    <p>{{ok ? 'yes' : 'no'}}</p>
+    <p>{{message.split('').reverse().join('') }}</p>
+    <p v-if="seen">现在你看到我了</p>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome hghhhhhh ',
+      rawHtml: '<span style="color:red">this should be red</span>',
+      color: 'blue',
+      number: 10,
+      ok: false,
+      message: 'vue',
+      seen: 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red{color: red;}
+.blue{color: blue; font-size: 100px;}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
