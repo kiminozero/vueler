433f06df955393ec4e3e634cc5ae0392a98c1019
zero <156142060@qq.com>
2020-04-03 04:27:42 +0800

    vue HelloW

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -16,84 +16,9 @@
       </div>
     </div>
     <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div @click="click3">
+      click3 me
+    </div>
   </div>
 </template>
 
@@ -121,7 +46,11 @@ export default {
     },
     doSomething: function () {
       console.log('"doSomething"')
-    }
+    },
+    click3: function () {
+      console.log('"click3"')
+      
+    },
   }
 }
 </script>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios'
+import VusAxios from 'vue-axios'
 Vue.config.productionTip = false
 
+Vue.use(VusAxios, axios)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
